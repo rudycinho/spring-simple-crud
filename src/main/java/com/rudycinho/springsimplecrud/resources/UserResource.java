6c120b3ecbd5f42658d1ea7d55a15d85ea1fbca7
r.martinez1 <r.martinez@empresasim.com>
2020-01-12 17:34:57 -0400

    Get created for UserResource

--- src/main/java/com/rudycinho/springsimplecrud/resources/UserResource.java
+++ src/main/java/com/rudycinho/springsimplecrud/resources/UserResource.java
@@ -1,12 +1,15 @@
 package com.rudycinho.springsimplecrud.resources;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +21,7 @@ import com.rudycinho.springsimplecrud.models.dto.UserDTO;
 import com.rudycinho.springsimplecrud.models.pojo.User;
 import com.rudycinho.springsimplecrud.models.vo.UserVO;
 import com.rudycinho.springsimplecrud.services.UserService;
+
 
 @RestController
 @RequestMapping("/api/user")
@@ -63,6 +67,41 @@ public class UserResource {
 			userService.delete(user);
 			UserDTO userDTO = new UserDTO(user);
 			response = new ResponseEntity<>(userDTO,HttpStatus.OK);
+		}
+		return response;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> get(@PathVariable("id")int id){
+		ResponseEntity<?> response;
+		User user = userService.get(id);
+		if(user==null) {
+			Map<String,String> errors = new HashMap<>();
+			errors.put("Errors", "The user does not exist");
+			response = new ResponseEntity<>(errors,HttpStatus.NOT_FOUND);
+		}else {
+			UserDTO userDTO = new UserDTO(user);
+			response = new ResponseEntity<>(userDTO,HttpStatus.OK);
+		}
+		return response;
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> get(){
+		ResponseEntity<?> response;
+		List<User> users = userService.getAll();
+		if(users.size()==0) {
+			Map<String,String> errors = new HashMap<>();
+			errors.put("Errors", "No users");
+			response = new ResponseEntity<>(errors,HttpStatus.NOT_FOUND);
+		}else {
+			List<UserDTO> usersDTO = new LinkedList<>();
+			UserDTO userDTO;
+			for(User user : users) {
+				userDTO = new UserDTO(user);
+				usersDTO.add(userDTO);
+			}
+			response = new ResponseEntity<>(usersDTO,HttpStatus.OK);
 		}
 		return response;
 	}
