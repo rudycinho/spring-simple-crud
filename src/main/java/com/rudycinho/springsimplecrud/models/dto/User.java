c67d4d62536738809361231a383811d17b01c0b4
r.martinez1 <r.martinez@empresasim.com>
2020-01-12 10:35:11 -0400

    User pojo created

--- src/main/java/com/rudycinho/springsimplecrud/models/dto/User.java
+++ src/main/java/com/rudycinho/springsimplecrud/models/dto/User.java
@@ -1,11 +1,40 @@
 package com.rudycinho.springsimplecrud.models.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
+@Table(name = "users")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id; 
+	
 	private String name;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(unique = true)
+	private String identification;
+	
+	@Column(nullable = true)
+	private String address;
+	
+	@Column(nullable = true)
+	private String phone;
+	
+	@Column(unique = true)
+	private String email;
+	
+	public User() {}
 }
