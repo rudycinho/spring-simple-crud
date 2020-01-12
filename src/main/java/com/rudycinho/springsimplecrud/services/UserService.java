2c792970a756b8d8f0bb870e63e1ecbbec39aa00
r.martinez1 <r.martinez@empresasim.com>
2020-01-12 15:19:32 -0400

    add Transactional to services class

--- src/main/java/com/rudycinho/springsimplecrud/services/UserService.java
+++ src/main/java/com/rudycinho/springsimplecrud/services/UserService.java
@@ -4,23 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rudycinho.springsimplecrud.models.pojo.User;
 import com.rudycinho.springsimplecrud.repositories.UserRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@Transactional
 	public User create(User user) {
 		return userRepository.save(user);
 	}
 	
+	@Transactional
 	public User update(User user) {
 		return userRepository.save(user);
 	}
 	
+	@Transactional
 	public void delete(User user) {
 		userRepository.delete(user);
 	}
