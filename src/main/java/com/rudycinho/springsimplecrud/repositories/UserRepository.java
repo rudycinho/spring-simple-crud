365eb17eef03fa12bce5a65e96c5bb9d33894b05
r.martinez1 <r.martinez@empresasim.com>
2020-01-12 13:34:06 -0400

    Reservation repository created

--- src/main/java/com/rudycinho/springsimplecrud/repositories/UserRepository.java
+++ src/main/java/com/rudycinho/springsimplecrud/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.rudycinho.springsimplecrud.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -7,9 +9,12 @@ import org.springframework.data.repository.query.Param;
 import com.rudycinho.springsimplecrud.models.pojo.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-	@Query("Select u from User u")
-	public User getAllUser();
+	@Query("Select u"+
+		   "from User u")
+	public List<User> getAllUser();
 	
-	@Query("Select u from User u where u.id =:id")
-	public User getUser(@Param("id") int id);
+	@Query("Select u"+
+	       "from User u"+
+		   "where u.id =:id")
+	public User getUser(@Param( "id") int id);
 }
