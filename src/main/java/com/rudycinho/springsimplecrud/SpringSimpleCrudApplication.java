241acd3fb2d72a6033fc4f64f2da89b41625329b
r.martinez1 <r.martinez@empresasim.com>
2020-01-11 23:30:37 -0400

    first commit, spring boot initializated

---
+++ src/main/java/com/rudycinho/springsimplecrud/SpringSimpleCrudApplication.java
@@ -0,0 +1,13 @@
+package com.rudycinho.springsimplecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rudycinho/springsimplecrud/SpringSimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudycinho.springsimplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
