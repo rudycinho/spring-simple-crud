3d206c74971a1d27f218401f3357bbadeef986c8
r.martinez1 <r.martinez@empresasim.com>
2020-01-26 11:12:54 -0400

    Setup spring security

--- src/main/java/com/rudycinho/springsimplecrud/SpringSimpleCrudApplication.java
+++ src/main/java/com/rudycinho/springsimplecrud/SpringSimpleCrudApplication.java
@@ -2,8 +2,11 @@ package com.rudycinho.springsimplecrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class
+})
 public class SpringSimpleCrudApplication {
 
 	public static void main(String[] args) {
